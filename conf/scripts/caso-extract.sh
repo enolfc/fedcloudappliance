b70b0842d0cf607cb11931ad0887e480a181f6b3
Enol Fernandez <enol.fernandez@egi.eu>
2017-08-08 10:22:13 +0200

    Upgrade caso version

--- conf/scripts/caso-extract.sh
+++ conf/scripts/caso-extract.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -v /etc/voms.json:/etc/voms.json \
+docker run -v /etc/voms.json:/etc/caso/voms.json \
            -v /etc/caso:/etc/caso \
            -v /var/spool/caso:/var/spool/caso \
            -v /var/spool/ssm:/var/spool/ssm \
