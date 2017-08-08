111223289e16e94f0f9e1ba7681f2c18f9bda8ea
Enol Fernandez <enol.fernandez@egi.eu>
2017-08-08 11:33:12 +0200

    Entry point to deal with image lists

---
+++ conf/cloudkeeper/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "x$DEBUG" = "x1" ] ; then
+    set -x
+fi
+
+EXTRA_OPTS=""
+
+IMAGE_LISTS_FILE=/etc/cloudkeeper/image-lists.conf
+if [ -f "$IMAGE_LISTS_FILE" ]; then
+    EXTRA_OPTS="$EXTRA_OPTS --image-lists=$(cat $IMAGE_LISTS_FILE | grep -v "^#" |  tr -s '\n' ' ')"
+fi
+
+if [ "x$BACKEND_PORT_50051_TCP_ADDR" != "x" ]; then
+    EXTRA_OPTS="$EXTRA_OPTS --backend-endpoint=$BACKEND_PORT_50051_TCP_ADDR:$BACKEND_PORT_50051_TCP_PORT"
+fi
+
+exec $@ $EXTRA_OPTS
