63a7340b6626728b4f1a3ed0594ba490c752ab80
Enol Fernandez <enol.fernandez@egi.eu>
2017-08-08 12:50:41 +0100

    Fix the location of apel volume

--- conf/scripts/caso-extract.sh
+++ conf/scripts/caso-extract.sh
@@ -3,5 +3,5 @@
 docker run -v /etc/voms.json:/etc/caso/voms.json \
            -v /etc/caso/caso.conf:/etc/caso/caso.conf \
            -v /var/spool/caso:/var/spool/caso \
-           -v /var/spool/ssm:/var/spool/ssm \
+           -v /var/spool/apel:/var/spool/apel \
            egifedcloud/caso
