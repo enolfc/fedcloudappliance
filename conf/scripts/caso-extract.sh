466b1ee208851514edfa61d866aaafe86b46e210
Enol Fernandez <enol.fernandez@egi.eu>
2018-05-02 00:01:45 +0100

    Use a specific caso voms.json file

--- conf/scripts/caso-extract.sh
+++ conf/scripts/caso-extract.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -v /etc/voms.json:/etc/caso/voms.json \
+docker run -v /etc/caso/voms.json:/etc/caso/voms.json \
            -v /etc/caso/caso.conf:/etc/caso/caso.conf \
            -v /var/spool/caso:/var/spool/caso \
            -v /var/spool/apel:/var/spool/apel \
