e8c267a2b8ade785f570712e83a111d9de96f22f
Enol Fernandez <enol.fernandez@egi.eu>
2018-05-05 01:05:27 +0300

    Remove what does not belong to the containers

--- accounting/ssm/ssm-send.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker run -v /etc/grid-security:/etc/grid-security \
-           -v /var/spool/apel:/var/spool/apel \
-           --rm egifedcloud/ssm ssmsend
--- cloudkeeper/core/cloudkeeper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run -v /etc/grid-security:/etc/grid-security \
-           -v /etc/cloudkeeper:/etc/cloudkeeper \
-           -v /image_data:/var/spool/cloudkeeper/images \
-           --link cloudkeeper-os:backend \
-           --rm egifedcloud/cloudkeeper cloudkeeper sync --debug 
